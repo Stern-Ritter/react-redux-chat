dd18ed53f3d828beffa9b908ff717c869919b392
Stern-Ritter <fadeew.a@mail.ru>
2022-02-16 21:42:20 +0300

    Add App component

---
+++ src/components/app/app.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ChatList from "../chat-list/chat-list";
+import ChatForm from "../chat-form/chat-form";
+import AppHeader from "../app-header/app-header";
+import { getMessages } from "../../services/actions";
+import styles from "./app.module.css";
+
+function App() {
+  const { hasError } = useSelector((store) => store.messages);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMessages());
+  }, []);
+
+  return (
+    <>
+      {hasError ? (
+        <>
+          <h1>Что-то пошло не так...</h1>
+          <p>В приложении произошла ошибка. Пожалуйста, перезагрузите страницу.</p>
+        </>
+      ) : (
+        <>
+          <AppHeader />
+          <main className={styles.main}>
+            <ChatList />
+            <ChatForm />
+          </main>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
