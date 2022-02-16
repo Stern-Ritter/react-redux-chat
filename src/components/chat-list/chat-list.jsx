1e97a17646f4f019cc04ba5bbc72715c707c4004
Stern-Ritter <fadeew.a@mail.ru>
2022-02-16 21:43:53 +0300

    Add ChatList component

---
+++ src/components/chat-list/chat-list.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import Message from "../message/message";
+import style from "./chat-list.module.css";
+
+function ChatList() {
+  const { loading, data } = useSelector((store) => store.messages);
+  return (
+    <>
+      <h1 className={style.title}>Сообщения: </h1>
+      {loading ? (
+        <p>Загрузка...</p>
+      ) : (
+        <ul className={style.list}>
+          {data.map((content, idx) => (
+            <Message key={idx} content={content}></Message>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default ChatList;
